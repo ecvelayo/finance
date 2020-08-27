315c889ff21a5d241995eab6c4c7e9f0b9fb0820
ecvelayo <ecvelayo@gmail.com>
2020-08-27 11:35:10 +0800

    Change

---
+++ src/https/ChargesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Increment\Finance\Http;
+
+use Illuminate\Http\Request;
+
+class ChargesController extends Controller
+{
+    //
+    public function addDeliveryCharge(Request $request){
+
+    }
+
+    public function addBankTransferCharge(Request $request){
+        
+    }
+}
