baf7f98a226258105b144fce1fdc24c8318f1450
ecvelayo <ecvelayo@gmail.com>
2020-09-01 14:27:19 +0800

    Effective date in fund transfer charges

---
+++ transfer/src/migrations/2020_09_01_062409_update_fund_transfer_charges_table_add_effective_date.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateFundTransferChargesTableAddEffectiveDate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('fund_transfer_charges', function (Blueprint $table){
+            $table->dateTime('effective_date')->after("maximum_amount");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
